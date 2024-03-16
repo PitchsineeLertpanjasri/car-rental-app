50f1e1609a147be6302a6c0783eff0007ca24fe3
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2024-03-17 03:09:39 +0700

    W06

--- src/components/LocationDateReserve.tsx
+++ src/components/LocationDateReserve.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react";
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs"
@@ -7,13 +8,19 @@ import { Select,MenuItem } from "@mui/material";
 
 
 export default function LocationDateReserve() {
+
+    const [reserveDate,setReserveDate] = useState(null)
+    const [location,setLocation] = useState('BKK')
+
     return(
         <div className="bg-slate-100 rounded-lg space-x-5 space-y-2 w-fit px-10 py-5 flex flex-row justify-center">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker className="bg-white"/>
+                <DatePicker className="bg-white" 
+                value={reserveDate} onChange={(value)=>{setReserveDate(value);alert(value)}}/>
             </LocalizationProvider>
 
-            <Select variant="standard"  name="location" id="location" className="h-[2em] w-[200px]">
+            <Select variant="standard"  name="location" id="location" className="h-[2em] w-[200px]" value={location}
+            onChange={(e)=>setLocation(e.target.value)}>
                 <MenuItem value="BKK">Bangkok</MenuItem>
                 <MenuItem value="CNX">Chaing Mai</MenuItem>
                 <MenuItem value="HKT">Phuket</MenuItem>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,11 +2,8 @@ import Image from 'next/image';
 import InteractiveCard from './InteractiveCard';
 
 
-export default function Productcard({carName,imgSrc}:{carName : string, imgSrc:string}) {
+export default function Productcard({carName,imgSrc,onCompare}:{carName : string, imgSrc:string,onCompare:Function}) {
 
-    function onCarSelected(){
-        alert("You Select "+ carName)
-    }
     return (
         <InteractiveCard contentName={carName}>
             <div className='w-full h-[70%] relative rounded-t-lg'>
@@ -16,7 +13,10 @@ export default function Productcard({carName,imgSrc}:{carName : string, imgSrc:s
                 className='object-cover rounded-t-lg'
                 />
             </div>
-            <div className='w-full h-[30%] p-[10px]'>{carName}</div>
+            <div className='w-full h-[15%] p-[10px]'>{carName}</div>
+            <button className='block h-[10%] text-sm rounded-md bg-sky-600
+            hover:bg-indigo-600 mx-2 px-1 py-1 shadow-sm text-white'
+            onClick={(e)=>{e.stopPropagation();onCompare(carName)}}>Compare</button>
         </InteractiveCard>
             
     );
