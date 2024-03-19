8afdb9effa1dc2e1324c1f9d24deb7193e7a97b8
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2024-03-19 21:03:25 +0700

    W09

--- src/app/(carinfo)/car/page.tsx
+++ src/app/(carinfo)/car/page.tsx
@@ -1,10 +1,24 @@
-import CarPanel from '@/components/CarPanel';
+import getCars from "@/libs/getCars";
+import CarCatalog from "@/components/CarCatalog";
+import { Suspense } from "react";
+import { LinearProgress } from "@mui/material";
+import CarPanel from "@/components/CarPanel";
 
 export default function Car(){
+    const cars = getCars()
+
     return (
         <main className="text-center p-5">
             <h1 className="tex-xl font-medium">Select Your Travel Parthner</h1>
+            <Suspense fallback={<p>Loading ... <LinearProgress/></p>}>
+                <CarCatalog carJson={cars}/>
+            </Suspense>
+
+            <hr className="my-10"/>
+
+            <h1 className="text-xl font-medium">Try Client-side Car Panel</h1>
             <CarPanel/>
+            
         </main>
     );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import InteractiveCard from './InteractiveCard';
 
 
-export default function Productcard({carName,imgSrc,onCompare}:{carName : string, imgSrc:string,onCompare:Function}) {
+export default function Productcard({carName,imgSrc,onCompare}:{carName : string, imgSrc:string,onCompare?:Function}) {
 
     return (
         <InteractiveCard contentName={carName}>
@@ -14,9 +14,12 @@ export default function Productcard({carName,imgSrc,onCompare}:{carName : string
                 />
             </div>
             <div className='w-full h-[15%] p-[10px]'>{carName}</div>
-            <button className='block h-[10%] text-sm rounded-md bg-sky-600
-            hover:bg-indigo-600 mx-2 px-1 py-1 shadow-sm text-white'
-            onClick={(e)=>{e.stopPropagation();e.preventDefault();onCompare(carName)}}>Compare</button>
+            {
+                onCompare? <button className='block h-[10%] text-sm rounded-md bg-sky-600
+                hover:bg-indigo-600 mx-2 px-1 py-1 shadow-sm text-white'
+                onClick={(e)=>{e.stopPropagation();e.preventDefault();onCompare(carName)}}>Compare</button>
+                :''
+            }   
         </InteractiveCard>
             
     );
