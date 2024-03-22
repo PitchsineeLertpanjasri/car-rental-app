4502345f94bf265c637349b61c5661d8b02d1b28
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2024-03-22 11:24:06 +0700

    W10

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import userLogin from "@/libs/userLogin";
 
 export const authOptions : AuthOptions = {
     providers: [
@@ -13,12 +14,15 @@ export const authOptions : AuthOptions = {
           // e.g. domain, username, password, 2FA token, etc.
           // You can pass any HTML attribute to the <input> tag through the object.
           credentials: {
-            username: { label: "Username", type: "text", placeholder: "username" },
+            email: { label: "Email", type: "email", placeholder: "email" },
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
+
+            if(!credentials) return null
+            const user = await userLogin(credentials.email,credentials.password)
             // Add logic here to look up the user from the credentials supplied
-            const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+            //const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
       
             if (user) {
               // Any object returned will be saved in `user` property of the JWT
@@ -32,8 +36,18 @@ export const authOptions : AuthOptions = {
           }
         })
     ],
-    session : {strategy:"jwt"}
+    session: { strategy: "jwt" },
+    callbacks:{
+      async jwt({token,user}){
+        return {...token,...user}
+      },
+      async session({session,token,user}){
+        session.user=token as any
+        return session
+
+      }
+    }
 }
 
 const handler = NextAuth(authOptions)
-export {handler as GET,handler as POST}
+export {handler as GET,handler as POST};
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -7,7 +7,7 @@ import { Link } from '@mui/material';
 
 export default async function TopMenu(){
 
-    const Session = await getServerSession(authOptions)
+    const session = await getServerSession(authOptions)
 
     return (
         <div className={styles.menucontainer}>
@@ -17,11 +17,11 @@ export default async function TopMenu(){
             <TopMenuItem title='About' pageRef='/about'/>
 
             {
-                Session? <Link href="/api/auth/signout">
+                session? <Link href="/api/auth/signout">
                     <div className='flex items-center absolute right-0 h-full px-2 text-cyan-600 text-sm'>
-                    Sign-Out of {Session.user?.name}</div></Link>
+                    Sign-Out of {session.user?.name}</div></Link>
                 :<Link href="/api/auth/signin"><div className='flex items-center absolute right-0 h-full px-2 text-cyan-600 text-sm'>Sign-In</div></Link>
             }
         </div>
-    );
+    )
 }
